5b5c179dfbe9aa78d1c9159502b77e6ae0499b2d
luisflr <lfloresr@ulasalle.edu.pe>
2026-08-30 19:06:19 -0500

    added navigations

--- cursos-frontend/nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ cursos-frontend/nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,19 +1,21 @@
 import {
   UserGroupIcon,
+  BanknotesIcon,
   HomeIcon,
   DocumentDuplicateIcon,
-} from '@heroicons/react/24/outline';
+} from "@heroicons/react/24/outline";
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  { name: "Home", href: "/dashboard", icon: HomeIcon },
   {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
+    name: "Invoices",
+    href: "/dashboard/invoices",
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
+  { name: "Payments", href: "/dashboard/payments", icon: BanknotesIcon },
 ];
 
 export default function NavLinks() {
